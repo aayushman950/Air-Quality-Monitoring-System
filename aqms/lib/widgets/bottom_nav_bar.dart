d07598e7288e4e6ede95cf976bf29a94a4176bf2
iusss <aayushman950@gmail.com>
2024-12-17 19:20:16 +0545

    put bottom navigation bar at the bottom

--- aqms/lib/widgets/bottom_nav_bar.dart
+++ aqms/lib/widgets/bottom_nav_bar.dart
@@ -11,27 +11,29 @@ class _BottomNavBarState extends State<BottomNavBar> {
   int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      selectedIndex: currentPageIndex,
-      onDestinationSelected: (index) {
-        setState(() {
-          currentPageIndex = index;
-        });
-      },
-      destinations: [
-        NavigationDestination(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.notifications),
-          label: 'Home',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.account_box_sharp),
-          label: 'Home',
-        ),
-      ],
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.notifications),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.account_box_sharp),
+            label: 'Home',
+          ),
+        ],
+      ),
     );
   }
 }
