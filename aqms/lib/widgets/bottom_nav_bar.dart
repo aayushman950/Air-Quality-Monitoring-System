84cb1a218d9059bcb13738c2fc5f602740b0ccea
iusss <aayushman950@gmail.com>
2024-12-17 19:31:22 +0545

    added different icon sizes for when selected and not selected

--- aqms/lib/widgets/bottom_nav_bar.dart
+++ aqms/lib/widgets/bottom_nav_bar.dart
@@ -21,18 +21,18 @@ class _BottomNavBarState extends State<BottomNavBar> {
         },
         destinations: [
           NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home, size: 40),
+            icon: Icon(Icons.home_outlined, size: 30),
             label: 'Home',
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.notifications),
-            icon: Icon(Icons.notifications_none_outlined),
+            selectedIcon: Icon(Icons.notifications, size: 40),
+            icon: Icon(Icons.notifications_none_outlined, size: 30),
             label: 'Notifications',
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.account_circle),
-            icon: Icon(Icons.account_circle_outlined),
+            selectedIcon: Icon(Icons.account_circle, size: 40),
+            icon: Icon(Icons.account_circle_outlined, size: 30),
             label: 'Account',
           ),
         ],
