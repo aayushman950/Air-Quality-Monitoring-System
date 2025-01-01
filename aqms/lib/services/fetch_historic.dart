1f34c08b66689d2138993c4adb83bab69a940cc6
iusss <aayushman950@gmail.com>
2025-01-01 16:56:58 +0545

    fix flask endpoint url

--- aqms/lib/services/fetch_historic.dart
+++ aqms/lib/services/fetch_historic.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:aqms/models/aqi_data_model.dart';
 
 Future<List<AQIData>> fetchHistoricalData() async {
-  final response = await http.get(Uri.parse('http://your-flask-server-url/history'));
+  final response = await http.get(Uri.parse('http://10.0.2.2:5000/history'));
 
   if (response.statusCode == 200) {
     List<dynamic> jsonData = jsonDecode(response.body);
